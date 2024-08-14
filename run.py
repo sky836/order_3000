892cde8fb7d8f77560f9e4031f362cd51f942db4
贵大孙波2002 <sky200210@outlook.com>
2024-08-14 11:43:37 +0800

    lr00003

--- run.py
+++ run.py
@@ -77,7 +77,7 @@ def main():
 
     # optimization有关
     parser.add_argument('--batch_size', type=int, default=128, help='batch size of train input data')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.00003, help='optimizer learning rate')
     parser.add_argument('--weight_decay', type=float, default=0.003, help='')
     parser.add_argument('--n_epochs', type=int, default=100, help='训练时迭代的次数')
     parser.add_argument('--patience', type=int, default=20, help='提前停止训练的忍耐的轮数')
